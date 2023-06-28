2bc1446c303b7e0169ee16657fb33567d01723c4
jacklrs <github@rushsoft.de>
2023-06-28 17:04:27 +0200

    feat: add blog about js;

--- src/blogposts/index.ts
+++ src/blogposts/index.ts
@@ -1,5 +1,12 @@
 export const blogposts = [
   {
+    id: "javascript-the-web-language",
+    created: "2023-06-26",
+    title: "PHP or javascript?",
+    description:
+      "Why we should consider to use more javascript for web development",
+  },
+  {
     id: "gut-feeling-estimates",
     created: "2018-10-05",
     title: "Gut feeling estimates",
