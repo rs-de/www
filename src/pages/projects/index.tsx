2db83914596b79404676c288a6c6114c3d483d24
jacklrs <github@rushsoft.de>
2023-08-17 11:36:02 +0200

    fix: layout;

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -17,8 +17,8 @@ export default function Projects({ markdowns }: { markdowns: string[] }) {
         <a href="#2023-3" id="2023-3">
           #
         </a>
-        <div className="w-full bg-[url('/img/projects/shopping-list-visual.webp')] bg-no-repeat border border-primary-6 rounded-2xl drop-shadow-md overflow-hidden">
-          <Box className="bg-gradient-to-br from-transparent to-primary-2  to-[50%] sm:to-[35%]">
+        <div className="w-full bg-[url('/img/projects/shopping-list-visual.webp')] bg-contain sm:bg-auto bg-no-repeat border border-primary-6 rounded-2xl drop-shadow-md overflow-hidden">
+          <Box className="bg-gradient-to-b sm:bg-gradient-to-br from-transparent to-primary-2  to-[25%] sm:to-[35%]">
             <div className="w-[255px] h-[315px] float-left" />
             <h2 className="!mt-0">Shopping-list App (2023)</h2>
             <MarkdownIntl>{markdowns[3]}</MarkdownIntl>
