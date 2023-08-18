f30fdf366ade6294c169886241d423009cef2e47
jacklrs <github@rushsoft.de>
2023-08-18 14:14:36 +0200

    fix: add missing SEO stuff

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,15 +1,32 @@
-import { useTranslations } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import NextHead from "next/head";
+import { usePathname } from "next/navigation";
 
 export default function Head() {
   const t = useTranslations();
+  const locale = useLocale();
+  const path = usePathname();
   return (
     <NextHead>
+      <link
+        rel="canonical"
+        href={`https://www.rushsoft.de${
+          path.startsWith(`/${locale}`) ? path : `/${locale}${path}`
+        }`}
+      />
       <title>{t("pageTitle")}</title>
       <meta
         name="description"
         content={t("pageDescription")}
         key="description"
+      />
+      <meta
+        name="author"
+        content="Jochen Probst - Web Application Development"
+      />
+      <meta
+        name="publisher"
+        content="Jochen Probst - Web Application Development"
       />
     </NextHead>
   );
