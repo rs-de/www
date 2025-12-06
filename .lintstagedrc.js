90fc149a29476f5f0aaf2765136629398cd21ef8
jacklrs <github@rushsoft.de>
2025-12-06 22:04:52 +0100

    chore: update deps

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,12 @@
-const path = require("path"); 
-const buildEslintCommand = (filenames) => 
-  `next lint --fix --file ${filenames 
-    .map((f) => path.relative(process.cwd(), f)) 
-    .join(" --file ")}`;
+const path = require("path");
 
-module.exports = { 
+const buildEslintCommand = (filenames) =>
+  `eslint --fix ${
+    filenames
+      .map((f) => `"${path.relative(process.cwd(), f)}"`)
+      .join(" ")
+  }`;
+
+module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
 };
