eb3e205dcff055d74e9d96df5cfb600bd3f8431d
jacklrs <github@rushsoft.de>
2025-12-06 22:43:40 +0100

    chore: fix upgrade issues esling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,16 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import prettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
-    ...nextVitals,
-    prettier,
-    // Override default ignores of eslint-config-next.
-    globalIgnores([
-        // Default ignores of eslint-config-next:
-        ".next/**",
-        "out/**",
-        "build/**",
-        "next-env.d.ts",
-    ]),
+  ...nextVitals,
+  prettier,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
 ]);
 
 export default eslintConfig;
